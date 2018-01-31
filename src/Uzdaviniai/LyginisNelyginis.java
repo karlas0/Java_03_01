cc078545a554ae634205f44cf37444ff8ae6da05
Karlas <karolis.zagarnauskas@gmail.com>
2018-01-31 10:40:35 +0200

    pack

--- src/Uzdaviniai/LyginisNelyginis.java
+++ src/Uzdaviniai/LyginisNelyginis.java
@@ -3,11 +3,16 @@ package Uzdaviniai;
 import java.util.Scanner;
 
 public class LyginisNelyginis {
-    public LyginisNelyginis() {
-        System.out.println("iveskite skaiciu");
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        if (a % 2 == 0) {
+    private int _a;
+
+    public int getA() {
+        return _a;
+    }
+
+    public void  setA(int value){_a = value;}
+    public void skaiciuok() {
+
+        if (_a % 2 == 0) {
             System.out.println("skaicius yra lyginis");
         } else {
             System.out.println("skaicius yra nelyginis");
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("iveskite skaiciu");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
 
         LyginisNelyginis objektas = new LyginisNelyginis();
+
+        objektas.setA(a);
+        System.out.println("ivestas skaicius: " +objektas.getA());
+        objektas.skaiciuok();
 	// write your code here
     }
 }
